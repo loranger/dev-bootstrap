82af9521e88c8c4e5bb95c5e966608096afebcc3
Laurent Goussard <loranger@free.fr>
2021-03-10 15:26:38 +0100

    :recycle: Simplify docker images

--- templates/init/symfony/init.sh
+++ templates/init/symfony/init.sh
@@ -2,7 +2,6 @@
 
 echo "Creating “$1” Symfony project"
 
-mkdir `slugify $1`
-cd `slugify $1`
-composer create-project symfony/website-skeleton App
+composer create-project symfony/website-skeleton `slugify $1`
 
+cd `slugify $1`

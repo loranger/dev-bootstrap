798d525f530944d245a6ca631fa94bd04d337fd9
Laurent Goussard <loranger@free.fr>
2023-01-24 14:05:04 +0100

    :sparkles: Select Prestashop version to install

--- templates/init/prestashop/init.sh
+++ templates/init/prestashop/init.sh
@@ -2,11 +2,32 @@
 
 echo "Creating “$1” Prestashop project"
 
-echo "Downloading latest PrestaShop, this may take a while ...";
+presta_versions=($(curl -s "https://api.github.com/repos/prestashop/prestashop/releases?per_page=9" | grep "tag_name" | cut -d '"' -f 4))
+if [[ ${#presta_versions[@]} -eq 0 ]]; then
+    echo "Error while fetching Prestashop version";
+    break;
+fi
 
-latest=$(curl -s https://api.github.com/repos/prestashop/prestashop/releases/latest | grep "browser_download_url.*zip" | cut -d '"' -f 4)
+PS3="Which Prestashop version do you require for “$1” project: "
+select version in "${presta_versions[@]}"; do
+    # Manually specified version
+    if [[ "$REPLY" == *"\."* || -z "${presta_versions[$REPLY]}" ]]; then
+        version=$REPLY
+    fi
 
-curl -s -L -o `slugify $1`.zip "$latest"
+    release=$(curl -s https://api.github.com/repos/prestashop/prestashop/releases/tags/$version);
+    if [[ "$release" == *"browser_download_url"* ]]; then
+        break;
+    else
+        echo "Could not find Prestashop v$version"
+    fi
+done
+
+echo "Downloading PrestaShop $version, this may take a while ...";
+
+download_url=$(echo $release | grep "browser_download_url.*zip" | cut -d '"' -f 4)
+
+curl -s -L -o `slugify $1`.zip "$download_url"
 unzip -qq -o `slugify $1`.zip -d `slugify $1`
 rm -f `slugify $1`.zip
 
